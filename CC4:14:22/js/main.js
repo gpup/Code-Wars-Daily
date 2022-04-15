39c5df00e6753cb149c6c90cbecdf07d546ab524
Seth Martin <jahgpup@gmail.com>
2022-04-15 12:20:23 -0600

    new code challenge

---
+++ CC4:14:22/js/main.js
@@ -0,0 +1,13 @@
+const arr = N => [ /* the numbers 0 to N-1 */ ];
+
+// Organize elements of array in ascending order
+arr.sort();
+// It should return N-1, or up to greatest number in array set
+
+function arr(N){
+    let arr = [];
+    for(let i =0; i < N; i++){
+        arr.push(i)
+    }
+    return arr;
+}
