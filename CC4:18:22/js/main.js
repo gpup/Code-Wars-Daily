c5b1d23bdd9973d27964ce73edd47dd19ea75b4b
Seth Martin <jahgpup@gmail.com>
2022-04-19 18:25:18 -0600

    code challenge for 4/18/22 this solution did not work

---
+++ CC4:18:22/js/main.js
@@ -0,0 +1,14 @@
+function rentalCarCost(d) {
+    let result;
+    if(d < 3){
+      result = (d*40);
+    }else if(d < 7){
+      result = ((d*40) - 20);
+    }else if(d >= 7){
+      result = ((d*40) - 50);
+    }else {
+      result = "Whatever";
+    }
+  }
+
+  //This did not work and I don't really know why
