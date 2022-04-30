fd89db32e048b737ae14180f8de75e91873ecd0e
Seth Martin <jahgpup@gmail.com>
2022-04-30 13:40:59 -0600

    pretty sure i can do this one

--- CC4:29:22/js/main.js
+++ CC4:29:22/js/main.js
@@ -1,21 +1,28 @@
-let languageDatabase = {english: 'Welcome',
-czech: 'Vitejte',
-danish: 'Velkomst',
-dutch: 'Welkom',
-estonian: 'Tere tulemast',
-finnish: 'Tervetuloa',
-flemish: 'Welgekomen',
-french: 'Bienvenue',
-german: 'Willkommen',
-irish: 'Failte',
-italian: 'Benvenuto',
-latvian: 'Gaidits',
-lithuanian: 'Laukiamas',
-polish: 'Witamy',
-spanish: 'Bienvenido',
-swedish: 'Valkommen',
-welsh: 'Croeso'};
+//Didn't quite get this one, but here is a working answer
+
 
 function greet(language) {
+
+    let languageDatabase = {english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    },
+
+defaultLanguage = 'english';
+return languageDatabase[language] || languageDatabase[defaultLanguage];
   
 } 
