04540b58e1765531e65bcd4b8ad11e101836bf84
Seth Martin <jahgpup@gmail.com>
2022-05-01 13:53:54 -0600

    need to become more familiar with toString as well as Number()

--- CC4:30:22/js/main.js
+++ CC4:30:22/js/main.js
@@ -1,3 +1,6 @@
 function squareDigits(num){
-    return 0;
-  }
+    //split square reduction and join num
+    return Number(num.toString().split('').map(elem => elem*elem).join(''))
+  };
+  
+  //Didn't get this one. Need to look into toString() and Number(). Also i first wanted to go with reduce. .map is correct.
