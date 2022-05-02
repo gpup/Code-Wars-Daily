b5f4b666c7a992c4430f06fef1fa7bb1d6a15f9b
Seth Martin <jahgpup@gmail.com>
2022-05-02 09:17:36 -0600

    my problem is this: 8kyu are too easy and 7kyu are too hard

---
+++ CC5:2:22.2/js/main.js
@@ -0,0 +1,11 @@
+
+// Nice easy one to bounce back from the last one
+
+function greet (name, owner) {
+    // if else to return the two messages
+    if(name===owner){
+      return 'Hello boss'
+    }else{
+      return 'Hello guest'
+    }
+  };
